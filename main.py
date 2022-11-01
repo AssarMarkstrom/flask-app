e78bb7a0a8e45d2caf1bf8fe244f58a7b2f95fee
assar markström <assarmarkstrom@gmail.com>
2022-11-01 09:19:16 +0100

    addded template

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ from flask import Flask, redirect, render_template
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
-"""
+
 class MyForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired(), Length(1, 20)])
-"""
+    name = StringField('name', validators=[DataRequired()])
+
 @app.route("/")
-def index():
-    return render_template("base.html")
+def submit():
+    form = MyForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('base.html', form=form)
+
 if __name__ == '__main__': 
     app.run() 
