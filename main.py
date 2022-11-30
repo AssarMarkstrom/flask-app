2dccb23656d1ba2170837f58bea090848470b1c8
Assar Markström <assarmarkstom@gmail.com>
2022-11-30 10:15:24 +0100

    add template

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
+# -*- coding: utf-8 -*-
+import secrets
 from flask import Flask, redirect, render_template
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
+SECRET_KEY = secrets.token_urlsafe(16) 
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+
 bootstrap = Bootstrap5(app)
 
-class MyForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-
-@app.route("/")
-def submit():
-    form = MyForm()
-    if form.validate_on_submit():
-        return redirect('/success')
-    return render_template('base.html', form=form)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__': 
     app.run() 
